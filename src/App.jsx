a49b6db28838cff645fbea3463ce62dc7f69b987
Karan Singh Manral <128299796+karan01ingh@users.noreply.github.com>
2025-08-18 12:37:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import React from 'react'
 import Signup from './pages/Signup.jsx';
 import Home from './pages/Home.jsx';
-import Navbar from './components/navbar.jsx';
+import Navbar from './components/Navbar.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
